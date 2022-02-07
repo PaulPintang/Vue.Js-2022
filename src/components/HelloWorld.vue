7515ab320d714d88f0265a3239cea76873cae9b3
puldyastin <puldyastin@gmail.com>
2022-02-07 21:23:02 +0800

    add buttons: quasar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -70,6 +70,16 @@
         :options="options"
         label="Square standout"
       />
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn color="white" text-color="black" label="Standard" />
+        <q-btn color="primary" label="Primary" />
+        <q-btn color="secondary" label="Secondary" />
+        <q-btn color="amber" glossy label="Amber" />
+        <q-btn color="brown-5" label="Brown 5" />
+        <q-btn color="deep-orange" glossy label="Deep Orange" />
+        <q-btn color="purple" label="Purple" />
+        <q-btn color="black" label="Black" />
+      </div>
     </div>
   </div>
 </template>
